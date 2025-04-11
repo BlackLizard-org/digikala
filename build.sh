f6b0852026f40793983775091ce4050ed3d5fac0
Black Lizard <ashafizadeh27@gmail.com>
2025-04-11 16:39:08 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -o errexit
 
-export DJANGO_SUPERUSER_USERNAME=admin
-export DJANGO_SUPERUSER_EMAIL=ali@ali.com
-export DJANGO_SUPERUSER_PASSWORD=ali137580
 
 pip install -r requirements.txt
 python manage.py migrate
