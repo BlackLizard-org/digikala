fddacfde0037dfa50c04fc54e9b618ad8f88e2a5
Black Lizard <ashafizadeh27@gmail.com>
2025-04-11 16:47:39 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 set -o errexit
 
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_EMAIL=admin@example.com
+export DJANGO_SUPERUSER_PASSWORD=admin123
 
 pip install -r requirements.txt
 python manage.py migrate
-python manage.py createsuperuser --noinput
+
+# فقط اگه کاربر وجود نداشت، ایجادش کن
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="$DJANGO_SUPERUSER_USERNAME").exists():
+    User.objects.create_superuser(
+        "$DJANGO_SUPERUSER_USERNAME",
+        "$DJANGO_SUPERUSER_EMAIL",
+        "$DJANGO_SUPERUSER_PASSWORD"
+    )
+EOF
+
 python manage.py collectstatic --noinput
