b53c257c92f85429967f13728e7c8dbcd915829d
Black Lizard <ashafizadeh27@gmail.com>
2025-04-18 11:45:44 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+set -e
+
+# Ensure python is available
+export PATH="/opt/render/project/.venv/bin:$PATH"
+
+# Run your Python build logic
+python3 script.py
+
+
+#!/bin/bash
 set -o errexit
 
 pip install -r requirements.txt
